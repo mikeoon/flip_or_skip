39aaaa87594f60f3e36212ee6dbf88f647af49f4
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-10-20 10:53:07 -0700

    add features and keys from scrape text

--- src/scrape_sx.py
+++ src/scrape_sx.py
@@ -1,50 +1,84 @@
 import re
 import requests
+import datetime as dt
+from numpy import nan
 from bs4 import BeautifulSoup
 
 
-stockx_url = 'https://stockx.com/air-jordan-10-retro-seattle'
-# will get 403 response without this
-headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15'}
+def _color_fill(detail_info, cc):
+	while cc < color_count:
+		cc+=1
+		detail_info[f'color_{cc}'] = nan()
+	return detail_info
 
 
+def _scrape_shoe(stockx_url):
+	r = requests.get(stockx_url, headers=headers)
 
-r = requests.get(stockx_url, headers=headers)
+	print(r)
 
-soup = BeautifulSoup(r.content, 'lxml')
+	soup = BeautifulSoup(r.content, 'lxml')
+	detail_info = {'name': None, 'style': None, 'm_color': None, 'retail_price': None, 
+				'release_date': None, 'num_sales': None, 'avg_sale': None}
 
-# shoe name
-shoe_name = soup.find('h1',{'class':'name'})
-print(shoe_name.text)
+	# shoe name
+	# Need to think of a way to get the name cleaned up into Brand, is it a retro?, actual name
+	shoe_name = soup.find('h1',{'class':'name'})
+	detail_info['name'] = shoe_name.text.lower()
 
-# gets details style, colorway, retail price, release date
-# NOT CLEANED YET
-detail_table = soup.find_all('div', {'class':'detail'})
-details = []
-for i in detail_table:
-	details.append(i.text.split(' '))
+	# gets details style, colorway, retail price, release date
+	# Clean but need to handle the colors
+	detail_table = soup.find_all('div', {'class':'detail'})
+
+	for i in detail_table:
+		d = i.text.lower().strip().split(' ')
+		# Breaks down the string into parts so better store data
+		if d[0] == 'retail':
+			detail_info['retail_price'] = float(d[2][1:])
+		elif d[0] == 'release':
+			temp = [int(t) for t in d[2].split('/')]
+			detail_info['release_date'] = dt.date(temp[2], temp[0], temp[1])
+		elif d[0] == 'colorway':
+			colors = i.text.lower().split('/')
+			detail_info['m_color'] = colors[0].split(' ')[1]
+			cc = 0
+			for i, c in enumerate(colors[1].split('-')):
+				detail_info[f'color_{i}'] = c
+				cc+=1
+			# Updates the color_{number} feature count to fill in later for df
+			if cc > color_count:
+				color_count = cc
+		else:
+			detail_info[d[0]] = d[1]
 
 
-# gets gauge table, num sales, price premium, avg sale price
-# NOT CLEANED YET
-gauge_table = soup.find_all('div', {'class':'gauge-container'})
-gauges = []
-for i in gauge_table:
-	gauges.append(i.text.split('$'))
+	# gets gauge table, num sales, price premium, avg sale price
+	# NOT CLEANED YET
+	gauge_table = soup.find_all('div', {'class':'gauge-container'})
 
+	for i in gauge_table:
+		d = i.text.lower().strip()
+		if '#' in d:
+			detail_info['num_sales'] = int(d[10:])
+		elif '$' in d:
+			detail_info['avg_sale'] = float(d[19:])
+
+	return _color_fill(detail_info, cc)
 
 
 
 # to test
+color_count = 1
 
-for d in details:
-	print(d)
+stockx_url = 'https://stockx.com/air-jordan-10-retro-seattle'
+#stockx_url =  'https://stockx.com/air-jordan-1-retro-high-unc-leather'
+# will get 403 response without this
+headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15'}
 
+shoe_info = _scrape_shoe(stockx_url)
 
-for g in gauges:
-	print(g)
-
-
+for k,v in shoe_info.items():
+	print(f'{k} = {v}')
 
 
 
