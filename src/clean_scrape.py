f32221cde28829f2e1eedf3ebcd48281e3a32c7c
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-11-02 14:32:09 -0700

    add remove duplicate shoes from df

--- src/clean_scrape.py
+++ src/clean_scrape.py
@@ -10,9 +10,21 @@ def combine_shoe_pkls(brand):
 	shoe_rows = [pd.read_pickle(shoe) for shoe in shoe_pkls]
 
 	df = pd.concat(shoe_rows, axis=0, sort=True)
-	df = df.drop('color_5', axis=1)
-	df = df.drop('sku', axis=1)
+	df = df.drop(['color_5', 'sku'], axis=1)
+	df = df.rename(columns={'avg_sale': 'avg_resale'}).sort_index()
 	return df.sort_index()
+
+
+
+def remove_duplicate_shoes(df):
+	duplicates = set()
+	dup_shoes = []
+	for i, shoe in df.iterrows():
+		if shoe['name'] not in duplicates:
+			duplicates.add(shoe['name'])
+		else:
+			df.drop(i, axis=0, inplace=True)
+	return df
 
 
 
