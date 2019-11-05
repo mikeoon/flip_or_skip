d133c541142531a7ebf538e3c902e786ac0e8b22
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-11-04 19:39:43 -0800

    add function to download bucket contents

--- src/bucket_interact.py
+++ src/bucket_interact.py
@@ -4,11 +4,37 @@ import numpy as np
 
 
 
+def download_imgs(brand):
+	s3 = boto3.client('s3')
+	shoe_set = set()
+	bucket_name = 'mikeoon-galvanize-bucket'
+	first = True
+	st_point = None
+	while len(shoe_set) != 29655:
+		if first:
+			request_shoes = s3.list_objects_v2(Bucket=bucket_name, Prefix=f'data/{brand}/')
+			first=False
+		else:
+			request_shoes = s3.list_objects_v2(Bucket=bucket_name, Prefix=f'data/{brand}/',StartAfter=st_point)
+		
+		keys = [shoe['Key'] for shoe in request_shoes['Contents']]
+		for i,k in enumerate(keys):	
+			if k not in shoe_set:
+				shoe_set.add(k)
+			if i >= (len(keys) - 1):
+				print(f'{k} is where it stopped')
+				st_point = k
+		print('Last stopping point')
+		print(len(shoe_set))
 
+	for s in shoe_set:
+		s3.download_file(bucket_name, s, s)
+		print(f'Downloaded {s}')
 
-
-
-
+def delete_broken_imgs(brand):
+	s3 = boto3.client('s3')
+	shoe_set = set()
+	bucket_name = 'mikeoon-galvanize-bucket'
 
 
 
