e8a5470e0c24120321c1866d406b6ee496476adb
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-11-08 10:34:39 -0800

    add function for full pipeline into models

--- src/shoe_pipeline.py
+++ src/shoe_pipeline.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
-
-
-def get_image_paths(df, num=1):
-	return [name.replace(' ', '_')+f'_img0{i}.jpg' for name in df['name']]
+import pickle
+from skimage import io, transform
 
 
 
 
-def dummy_colors(df, mcolor=True, ncolor=True, num_colors=1):
+def _dummy_colors(df, mcolor=True, ncolor=True, num_colors=1):
 	if mcolor:
 		u_m_colors = df['m_color'].unique()
 		
@@ -20,20 +18,31 @@ def dummy_colors(df, mcolor=True, ncolor=True, num_colors=1):
 			u_color = df[f'color_{i}'].unique()
 			for c in u_color:
 				if type(c) != type(1.0):
-					df[f'color_{i}_'+(c.replace(' ','_'))] = (df[f'color_{i}'] == c) *1
+					df[f'color_{i}_'+(c.replace(' ','_').strip())] = (df[f'color_{i}'] == c) *1
 	
 	return df
 
 # Dummys the cuts 
-def dummy_cut(df):
+def _dummy_cut(df):
 	cuts = df['cut'].unique()
 	for c in cuts:
 		if c != 0:
 			df[f'cut_{c}'] = (df['cut'] == c) * 1
 	return df
 
+
+def _dummy_release_date(df):
+	df['release_day'] = [-1 if d is None else d.weekday() for d in df['release_date']]
+	# Days are released by 0 - 6, 0 = monday
+	for i in range(7):
+		df[f'release_{i}'] = (df['release_day'] == i)*1
+
+	return df.drop('release_day', axis=1, inplace=True)
+
+
+
 # Hard coded, this is for my first model to clean up patchy data
-def patch_up_m_color(df, brand):
+def _patch_up_m_color(df, brand):
 	if brand == 'nike':
 		df.loc[91, 'm_color'] = 'red'
 		df.loc[128, 'm_color'] = 'multi-color'
@@ -55,6 +64,38 @@ def patch_up_m_color(df, brand):
 		df.loc[979, 'color_1'] = 'gum'
 
 	return df
+
+def create_img_df(df):
+	image_df = df[['name', 'flip']].copy()
+	image_df['img']=[name.replace(' ', '_')+f'_img01.jpg' for name in df['name']]
+	return image_df
+
+def pkle_img(df, brand):
+	df['resize_img'] = [transform.resize(io.imread('data/nike/'+shoe), (500, 500, 3)) for shoe in df['img']]
+	with open(f'data/{brand}/resized_imgs.pkl', 'wb') as f:
+		pickle.dump(df, f)
+
+
+
+def flip_skip_pipeline(df, brand, num_c=2):
+	
+	if brand == 'nike':
+		X_cols = ['name', 'cut', 'retail_price', 'm_color', 'color_0', 'color_1', 'release_date']
+	elif brand =='air_jordan':
+		X_cols = ['name', 'cut', 'retail_price','retro', 'm_color', 'color_0', 'color_1', 'release_date']
+	target = ['flip']
+	drop_cols = ['name', 'cut', 'm_color', 'color_0', 'color_1', 'release_date']
+	
+	model_df = df[X_cols].copy()
+	model_targets = df['flip'].copy() * 1
+	_patch_up_m_color(model_df, brand)
+	_dummy_colors(model_df, num_colors=num_c)
+	_dummy_cut(model_df)
+	_dummy_release_date(model_df)
+
+	return model_df.drop(drop_cols, axis=1), model_targets
+
+
 
 
 
